7060994bdc4c8d01ff84e40e82fb4bf3a410da8e
Yisaaaa <ryansanisit19@gmail.com>
2023-08-28 17:25:36 +0800

    some code style update

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const clickSound = new Audio();
 clickSound.src = "audio/click.mp3";
 
 startBtnEL.addEventListener("click", function () {
-  clickSound.play();
-  // Sets a timeout for 1 sec
-  setTimeout(() => {
-    // Hide the start page
-    hide(startPageEL);
+    clickSound.play();
+    // Sets a timeout for 1 sec
+    setTimeout(() => {
+        // Hide the start page
+        hide(startPageEL);
 
-    // Show the main page
-    show(mainEL);
+        // Show the main page
+        show(mainEL);
 
-    // Initialize the game
-    const game = new Game();
-    game.start();
-  }, 1000);
+        // Initialize the game
+        const game = new Game();
+        game.start();
+    }, 1000);
 });
 
 // Create a function that plays a round of the game
@@ -47,101 +47,101 @@ startBtnEL.addEventListener("click", function () {
 // }
 
 function hide(element) {
-  element.style.opacity = "0";
-  element.style.pointerEvents = "none";
-  element.style.visibility = "hidden";
+    element.style.opacity = "0";
+    element.style.pointerEvents = "none";
+    element.style.visibility = "hidden";
 }
 
 function show(element) {
-  element.style.opacity = "1";
-  element.style.pointerEvents = "auto";
-  element.style.visibility = "visible";
+    element.style.opacity = "1";
+    element.style.pointerEvents = "auto";
+    element.style.visibility = "visible";
 }
 
 class Game {
-  #defeatsWhat = { rock: "scissors", paper: "rock", scissors: "paper" };
-  #WEAPONS = ["rock", "paper", "scissors"];
-  constructor() {
-    this.hScore = 0;
-    this.cScore = 0;
-  }
-
-  // Returns a boolean if there is a winner already
-  isThereAWinner() {
-    return this.hScore === 5 || this.cScore === 5;
-  }
-
-  // Returns a number between min and max. Min is inclusive.
-  getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-  // Updates the score depending on the result of the round.
-  updateScore(result) {
-    if (result === "win") {
-      this.hScore++;
-      document.querySelector(".you-score").textContent = this.hScore;
-    } else if (result === "lose") {
-      this.cScore++;
-      document.querySelector(".computer-score").textContent = this.cScore;
+    #defeatsWhat = { rock: "scissors", paper: "rock", scissors: "paper" };
+    #WEAPONS = ["rock", "paper", "scissors"];
+    constructor() {
+        this.hScore = 0;
+        this.cScore = 0;
     }
-  }
 
-  // Plays a round of a game
-  playRound(player, computer) {
-    if (player === computer) {
-      return "tie";
-    } else if (this.#defeatsWhat[player] === computer) {
-      return "win";
-    } else {
-      return "lose";
+    // Returns a boolean if there is a winner already
+    isThereAWinner() {
+        return this.hScore === 5 || this.cScore === 5;
     }
-  }
 
-  whoWon() {
-    if (this.hScore === 5) {
-      return "human";
-    } else {
-      return "computer";
+    // Returns a number between min and max. Min is inclusive.
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
     }
-  }
 
-  declareWinner() {
-    const weaponsEL = document.querySelector(".weapons");
-    const winnerStatement = document.querySelector(".choose");
-
-    hide(weaponsEL);
-
-    winnerStatement.style.transform = "translateY(-22rem)";
-    winnerStatement.style.fontSize = "4.8rem";
-    if (this.whoWon() === "human") {
-      winnerStatement.textContent = "Congratulations, human.";
-    } else {
-      winnerStatement.textContent = "You failed humanity.";
-    }
-  }
-
-  start() {
-    let result;
-    const weapons = document.querySelectorAll(".weapon");
-    weapons.forEach((weapon) => {
-      weapon.addEventListener("click", () => {
-        clickSound.play();
-
-        const human = weapon.value;
-        const computer =
-          this.#WEAPONS[this.getRandomInt(0, this.#WEAPONS.length)];
-
-        console.log(`human: ${human}`);
-        console.log(`computer: ${computer}`);
-
-        result = this.playRound(human, computer);
-        this.updateScore(result);
-
-        if (this.isThereAWinner()) {
-          this.declareWinner();
+    // Updates the score depending on the result of the round.
+    updateScore(result) {
+        if (result === "win") {
+            this.hScore++;
+            document.querySelector(".you-score").textContent = this.hScore;
+        } else if (result === "lose") {
+            this.cScore++;
+            document.querySelector(".computer-score").textContent = this.cScore;
         }
-      });
-    });
-  }
+    }
+
+    // Plays a round of a game
+    playRound(player, computer) {
+        if (player === computer) {
+            return "tie";
+        } else if (this.#defeatsWhat[player] === computer) {
+            return "win";
+        } else {
+            return "lose";
+        }
+    }
+
+    whoWon() {
+        if (this.hScore === 5) {
+            return "human";
+        } else {
+            return "computer";
+        }
+    }
+
+    declareWinner() {
+        const weaponsEL = document.querySelector(".weapons");
+        const winnerStatement = document.querySelector(".choose");
+
+        hide(weaponsEL);
+
+        winnerStatement.style.transform = "translateY(-22rem)";
+        winnerStatement.style.fontSize = "4.8rem";
+        if (this.whoWon() === "human") {
+            winnerStatement.textContent = "Congratulations, human.";
+        } else {
+            winnerStatement.textContent = "You failed humanity.";
+        }
+    }
+
+    start() {
+        let result;
+        const weapons = document.querySelectorAll(".weapon");
+        weapons.forEach((weapon) => {
+            weapon.addEventListener("click", () => {
+                clickSound.play();
+
+                const human = weapon.value;
+                const computer =
+                    this.#WEAPONS[this.getRandomInt(0, this.#WEAPONS.length)];
+
+                console.log(`human: ${human}`);
+                console.log(`computer: ${computer}`);
+
+                result = this.playRound(human, computer);
+                this.updateScore(result);
+
+                if (this.isThereAWinner()) {
+                    this.declareWinner();
+                }
+            });
+        });
+    }
 }
